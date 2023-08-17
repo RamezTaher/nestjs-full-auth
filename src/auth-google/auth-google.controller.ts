641622d4c4865b02aa40cfae29833b6f14c46d0e
Ramez Taher <ramezgamer11@gmail.com>
2023-08-18 00:46:09 +0100

    feat: finish the google 0auth20

--- src/auth-google/auth-google.controller.ts
+++ src/auth-google/auth-google.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { GoogleAuthGuard } from './guards/google.guard';
+import { Request } from 'express';
+import { Routes } from 'src/utils/constants';
 
-@Controller('auth-google')
-export class AuthGoogleController {}
+@Controller(Routes.AUTH)
+export class AuthGoogleController {
+  @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
+  handleLogin() {
+    return { msg: 'Google Authentication' };
+  }
+
+  // api/auth/google/redirect
+  @Get('google/redirect')
+  @UseGuards(GoogleAuthGuard)
+  handleRedirect() {
+    return { msg: 'OK' };
+  }
+
+  @Get('status')
+  user(@Req() request: Request) {
+    console.log(request.user);
+    if (request.user) {
+      return { msg: 'Authenticated' };
+    } else {
+      return { msg: 'Not Authenticated' };
+    }
+  }
+}
--- src/social/types/social.type.ts
+++ src/social/types/social.type.ts
@@ -1,4 +1,5 @@
 export type SocialType = {
+  id: string;
   firstName?: string;
   lastName?: string;
   email?: string;
