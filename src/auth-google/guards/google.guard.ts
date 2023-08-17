da258dbe00bcc8e2e6a4cecb1afc1e371c9132b1
Ramez Taher <ramezgamer11@gmail.com>
2023-08-18 00:47:10 +0100

    feat: adding the google  guard

---
+++ src/auth-google/guards/google.guard.ts
@@ -0,0 +1,12 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class GoogleAuthGuard extends AuthGuard('google') {
+  async canActivate(context: ExecutionContext) {
+    const activate = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+    await super.logIn(request);
+    return activate;
+  }
+}
