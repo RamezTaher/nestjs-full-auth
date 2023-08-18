c105370aa3ea62f63406242beadbefedc7fe0c1b
Ramez Taher <ramezgamer11@gmail.com>
2023-08-18 14:55:41 +0100

    refactor: keep one single status endpoint

--- src/auth-google/auth-google.controller.ts
+++ src/auth-google/auth-google.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get, UseGuards, Request, Inject } from '@nestjs/common';
 import { Routes, Services } from 'src/utils/constants';
 import { GoogleAuthGuard } from './guards/google.guard';
-import { AuthGuard } from '@nestjs/passport';
-import { NullableType } from 'src/utils/types/nullable.type';
-import { User } from 'src/users/entities/user.entity';
+
 import { IAuthService } from 'src/auth/auth';
 import { AuthProvidersEnum } from 'src/auth/enums/auth-providers.enum';
 
@@ -31,11 +29,5 @@ export class AuthGoogleController {
     );
 
     return user;
-  }
-
-  @Get('status')
-  @UseGuards(AuthGuard('jwt'))
-  public status(@Request() request): Promise<NullableType<User>> {
-    return this.authService.status(request.user);
   }
 }
